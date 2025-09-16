82bd956ca650119435613605f25e0b2acf4c5b52
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-09-16 18:10:33 +0700

    Correct main-class

--- Task_1_1/src/main/java/Main.java
+++ Task_1_1/src/main/java/Main.java
@@ -3,16 +3,17 @@ class Main {
     public static void main(String[] args) {
         int[] arr = new int[] {6, 5, 4, 3, 2, 1};
         System.out.println("--Unsorted array--");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+        printArray(arr);
+        HeapSort.sortArr(arr);
+        System.out.println("--Sorted array--");
+        printArray(arr);
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
             System.out.print(" ");
         }
         System.out.print('\n');
-        HeapSort.sortArr(arr);
-        System.out.println("Sorted array");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            System.out.print(" ");
-        }
     }
 }
