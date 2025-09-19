2b977b0dc947eeba3d36ba1243b2104520695736
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-09-19 21:53:15 +0700

    New tests

--- Task_1_2/src/test/java/BckJckTesting.java
+++ Task_1_2/src/test/java/BckJckTesting.java
@@ -88,4 +88,59 @@ class BckJckTesting {
 
         assert (state.sum == 10);
     }
+
+    @Test
+    void testingLeader1() {
+        Card two = new Card(CardTypes.TWO, Suit.H);
+        Card three = new Card(CardTypes.THREE, Suit.H);
+        Card four = new Card(CardTypes.FOUR, Suit.H);
+        Card five = new Card(CardTypes.FIVE, Suit.H);
+        Card six = new Card(CardTypes.SIX, Suit.H);
+        Card seven = new Card(CardTypes.SEVEN, Suit.H);
+        Card eight = new Card(CardTypes.EIGHT, Suit.H);
+        Card nine = new Card(CardTypes.NINE, Suit.H);
+        Card ten = new Card(CardTypes.TEN, Suit.H);
+        Card jack = new Card(CardTypes.JACK, Suit.H);
+        Card queen = new Card(CardTypes.QUEEN, Suit.H);
+        Card king = new Card(CardTypes.KING, Suit.H);
+        two.isOpen = true;
+        three.isOpen = true;
+        four.isOpen = true;
+        five.isOpen = true;
+        six.isOpen = true;
+        seven.isOpen = true;
+        eight.isOpen = true;
+        nine.isOpen = true;
+        ten.isOpen = true;
+        jack.isOpen = true;
+        queen.isOpen = true;
+        king.isOpen = true;
+        BlackJackLeader.nameCard(two);
+        BlackJackLeader.nameCard(three);
+        BlackJackLeader.nameCard(four);
+        BlackJackLeader.nameCard(five);
+        BlackJackLeader.nameCard(six);
+        BlackJackLeader.nameCard(seven);
+        BlackJackLeader.nameCard(eight);
+        BlackJackLeader.nameCard(nine);
+        BlackJackLeader.nameCard(ten);
+        BlackJackLeader.nameCard(jack);
+        BlackJackLeader.nameCard(queen);
+        BlackJackLeader.nameCard(king);
+    }
+
+    @Test
+    void testingPartState1() {
+        PartState state = new PartState();
+        state.addNewCard(new Card(CardTypes.ACE, Suit.H));
+        state.updateSum();
+        state.addNewCard(new Card(CardTypes.ACE, Suit.D));
+        state.updateSum();
+        state.addNewCard(new Card(CardTypes.ACE, Suit.C));
+        state.updateSum();
+        state.addNewCard(new Card(CardTypes.ACE, Suit.D));
+        state.updateSum();
+
+        assert (state.sum == 14);
+    }
 }
