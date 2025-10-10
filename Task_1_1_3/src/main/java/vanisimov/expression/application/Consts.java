3fefbbf40b597bee4aefe3c93f2e1af65bbc32c4
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-10-10 22:01:25 +0700

    C

--- Task_1_1_3/src/main/java/vanisimov/expression/application/Consts.java
+++ Task_1_1_3/src/main/java/vanisimov/expression/application/Consts.java
@@ -3,8 +3,8 @@ package vanisimov.expression.application;
 class Consts {
 
     static final String greeting = "It's an expression explorer\n";
-    static final String userInput = "If your expression is in a file, enter [1]. " +
-            "Other input data will be interpreted as you chose stdIn\n";
+    static final String userInput = "If your expression is in a file, enter [1]. "
+            + "Other input data will be interpreted as you chose stdIn\n";
     static final String inputExp = "Your expression: ";
     static final String inputVar = "Input variable for derivativing: ";
     static final String showDer = "exp'(%s) = ";
